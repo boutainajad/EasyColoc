59d7dce9d6600735692853273276720793e55d89
boutaina jad <boutaina.jad.2020@gmail.com>
2026-02-26 15:26:37 +0000

    ajoute des routes et form requests

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,35 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ColocationController;
+use App\Http\Controllers\InvitationController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ExpenseController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('auth')->group(function () {
+
+    // Colocations
+    Route::get('/colocations', [ColocationController::class, 'index'])->name('colocations.index');
+    Route::get('/colocations/create', [ColocationController::class, 'create'])->name('colocations.create');
+    Route::post('/colocations', [ColocationController::class, 'store'])->name('colocations.store');
+    Route::get('/colocations/{colocation}', [ColocationController::class, 'show'])->name('colocations.show');
+    Route::get('/colocations/{colocation}/edit', [ColocationController::class, 'edit'])->name('colocations.edit');
+    Route::put('/colocations/{colocation}', [ColocationController::class, 'update'])->name('colocations.update');
+    Route::post('/colocations/{colocation}/cancel', [ColocationController::class, 'cancel'])->name('colocations.cancel');
+    Route::post('/colocations/{colocation}/leave', [ColocationController::class, 'leave'])->name('colocations.leave');
+
+    // Invitations
+    Route::post('/colocations/{colocation}/invite', [InvitationController::class, 'store'])->name('invitations.store');
+    Route::get('/invitations/{token}/accept', [InvitationController::class, 'accept'])->name('invitations.accept');
+    Route::get('/invitations/{token}/refuse', [InvitationController::class, 'refuse'])->name('invitations.refuse');
+
+    // Categories
+    Route::post('/colocations/{colocation}/categories', [CategoryController::class, 'store'])->name('categories.store');
+    Route::delete('/categories/{category}', [CategoryController::class, 'destroy'])->name('categories.destroy');
+
+    // Expenses
+    Route::post('/colocations/{colocation}/expenses', [ExpenseController::class, 'store'])->name('expenses.store');
+    Route::put('/expenses/{expense}', [ExpenseController::class, 'update'])->name('expenses.update');
+    Route::delete('/expenses/{expense}', [ExpenseController::class, 'destroy'])->name('expenses.destroy');
+
 });
